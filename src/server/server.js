8ee94af168847a01c044ac9ee85187dba8c287d1
Nikki Everett <nikki.everett@outlook.com>
2017-07-14 22:14:44 -0500

    build deployed successfully.. still need to change db routing for deployment environment

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,6 @@
 import * as firebase from 'firebase';
 var moment = require('moment');
 
-moment().format();
-
 var config = {
   apiKey: "AIzaSyCO1Zp5x6tt9jSZU8LrzmfdVIVqKsiA-7Q",
   authDomain: "react-chat-ui-8198a.firebaseapp.com",
@@ -10,14 +8,5 @@ var config = {
 };
 
 firebase.initializeApp(config);
-
-// const port = process.env.PORT || 5003;
-//
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-//
-// app.listen(port, function(){
-//   console.log('listening on port', port);
-// });
-
 
 export default firebase;
